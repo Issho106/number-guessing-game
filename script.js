daedbdbf2fc3fa44969e94590eb9b83de862d97a
Graham Robert <grahamrobert2004@gmail.com>
2026-06-24 15:32:04 +0200

    Built further on initial game logic | Added game(), checkGuess(), getPlayerGuess() | Added counter to keep track of tries

--- script.js
+++ script.js
@@ -1,28 +1,73 @@
-function generateRandom() {
-    return Math.round(Math.random() * 100) + 1;
+function generateRandomNumber() {
+    return Number(Math.round(Math.random() * 100) + 1);
 }
+
+function game() {
+    let randomNr = generateRandomNumber();
+    let count = 10;
+    for(let i = 0; i < 10; i++){
+        console.log(count + " tries left");
+
+        let pGuess = getPlayerGuess();
+
+        if(isNaN(pGuess)) {
+            console.log("Goodbye!");
+            return;
+        }
+
+        let res = checkGuess(pGuess, randomNr);
+        
+        if(res == "CORRECT!") {
+            console.log(res + "You win the memory of once guessing a random number between 1-100 <3");
+            return;
+        }
+        else {
+            console.log(res);
+            count--;
+
+            if(count == 0){
+                console.log("You lose, the number was " + randomNr);
+            }
+        }
+    }
+}
+
+function checkGuess(_guess, _answer){
+
+    if (_guess > _answer) {
+        return _guess + " is wrong! Hint: You guessed high";
+    }
+    else if (_guess < _answer) {
+        return _guess + " is wrong! Hint: You guessed low";
+    }
+    return "CORRECT!";
+}
+
+function getPlayerGuess() {
+    let isInvalid;
+    let guess;
+    do {
+        isInvalid = false;
+        let input = prompt("Guess a number between 1-100 (Type any character to exit):");
+        guess = Number(input);
+        if(guess < 1 || guess > 100){
+            console.log("Make sure you choose a number between 1 - 100");
+            isInvalid = true;
+        } else if (guess === null) {
+            console.log("Please enter a number to guess, or any character to exit.");
+            isInvalid = true;
+        } else if (isNaN(guess)) { return NaN ;}
+    }while(isInvalid);
+
+    return guess;
+}
+
 
 let again;
 
 do {
-    console.clear(); //works
+    console.clear();
+    game();
+    again = prompt("Would you like to go again? (y/n):");
+}while(again.toLowerCase() == 'y');
 
-    let randomNr = generateRandom();
-    let guess = Number(prompt("Guess a number between 1-100:"));
-
-    if(guess < 1 || guess > 100){
-        console.log("Make sure you choose a number between 1 - 100");
-    }
-    if(isNaN(guess)){
-        console.log("Please enter only NUMBERS between 1 - 100");
-    } 
-
-    if(guess == randomNr) {
-        console.log("Win!");
-    }
-    else {
-        console.log("Lose... the number was: " + randomNr);
-    }
-
-    again = prompt("Go again? (y/n):");
-}while(again == 'y');
